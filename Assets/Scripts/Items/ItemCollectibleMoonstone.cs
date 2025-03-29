73d3d3b2089dafd9e58a071c23720ea73907373a
André Victor Gonçalves Nascimento <135927008+AndreVictoN@users.noreply.github.com>
2025-03-29 19:30:14 -0300

    Update and rename ItemCollectableMoonstone.cs to ItemCollectibleMoonstone.cs

--- Assets/Scripts/Items/ItemCollectibleMoonstone.cs
+++ Assets/Scripts/Items/ItemCollectibleMoonstone.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemCollectableMoonstone : ItemCollectableBase
+public class ItemCollectibleMoonstone : ItemCollectibleBase
 {
     protected override void OnCollect()
     {
